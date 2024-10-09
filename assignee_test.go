c0812efd47f1201d93fbe8a547682dfdda83a3f1
BabakDoraniarab <babakdoraniarab@jajafinance.com>
2024-10-09 11:20:28 +0300

    TODOs are added for assignee add func

--- assignee_test.go
+++ assignee_test.go
@@ -1,10 +1,30 @@
-package main 
+package main
+
+import "testing"
+
+// TODO test add func
+func TestAdd(t *testing.T){
+	//01 we need to create a list of assingess
+	var assignees AssigneeList
+
+	// create a new assignee and test it is created correctly or not
+	// TODO create a new assignee
+
+	// TODO check name 
+
+	//TODO check email 
+
+	//TODO check created_at and updated at are empty or not
+
+	//TODO check assigne list has the new assignee or not
 
 
 
+	
 
-// TODO test add func 
 
+
+}
 
 
 // TODO test validate func
