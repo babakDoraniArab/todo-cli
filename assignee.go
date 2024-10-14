57be73ac9fba19186ac0bc5500e3934609ac6f3d
BabakDoraniarab <babakdoraniarab@jajafinance.com>
2024-10-14 09:22:18 +0300

    assinee funcs are completed

--- assignee.go
+++ assignee.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 	"time"
 )
@@ -16,6 +17,19 @@ type Assignee struct {
 type AssigneeList []Assignee
 
 var assignees AssigneeList
+
+func (assignees AssigneeList) showAll() {
+	for index, value := range assignees {
+		fmt.Printf("id is = %d , assigne name is %s and his/her email is %s and created at %s and updated at %s\n\n",
+			index,
+			value.name,
+			value.email,
+			value.created_at.Format(time.RFC3339),
+			value.updated_at.Format(time.RFC1123),
+		)
+	}
+
+}
 
 func (assignees *AssigneeList) add(title string, email string) Assignee {
 
@@ -37,8 +51,36 @@ func (assignees *AssigneeList) add(title string, email string) Assignee {
 }
 
 // TODO edit assignee
+func (assignees *AssigneeList) editAssignee(index int, name string, email string) Assignee {
+	validation, err := assignees.validateAssigneesIndex(index)
+	if !validation {
+		panic(err)
+	}
+	(*assignees)[index].name = name
+	(*assignees)[index].email = email
+
+	//TODO save to file should call in here
+
+	return (*assignees)[index]
+}
 
 // TODO delete assignee
+func (assignees *AssigneeList) deleteAssignee(index int) {
+	*assignees = append((*assignees)[:index], (*assignees)[index+1:]...)
+	assignees.showAll()
+
+}
+func (assignees *AssigneeList) validateAssigneesIndex(index int) (bool, error) {
+	var err error
+	if index < 0 || index > len(*assignees) {
+		err = errors.New("the assignee ID is incorrect")
+
+	}
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
 
 func (assignees *AssigneeList) validateAssignee(title string, email string) (bool, error) {
 
--- main.go
+++ main.go
@@ -1,20 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 func main() {
-	assignees.add("b", "babak.dorani@gmail.com")
+	assignees.add("bbbb", "babak.dorani@gmail.com")
 	assignees.add("babak2", "babak4.dorani@gmail.com")
 	assignees.add("babak3", "babak4.dorani@gmail.com")
-	for _, value := range assignees {
-		fmt.Printf("assigne name is %s and his/her email is %s and created at %s and updated at %s\n\n",
-			value.name,
-			value.email,
-			value.created_at.Format(time.RFC3339),
-			value.updated_at.Format(time.RFC1123),
-		)
-	}
+	assignees.editAssignee(0, "ali", "ali.test@gmail.com")
+	assignees.showAll()
+
 }
